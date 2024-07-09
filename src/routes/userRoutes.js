2475742726e4b2085a91284945da6277114c1192
Ludhiya <ludhiyargiji1@gmail.com>
2024-07-09 21:27:09 +0800

    Added middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
 router.post("/users/register", userController.createUser);
 router.post("/users/login", userController.loginUser);
-router.delete("/users/:id", userController.deleteUser);
+router.delete("/users/:id", auth, userController.deleteUser);
 
 module.exports = router;
