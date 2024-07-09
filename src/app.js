5d799386fea9126d5ae4ce8a6495bb95e2bcd6aa
Ludhiya <ludhiyargiji1@gmail.com>
2024-07-09 21:15:06 +0800

    -

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 
+// const crypto = require("crypto");
+// const secret = crypto.randomBytes(64).toString("hex");
+// console.log(secret);
+
 const app = express();
 app.use(bodyParser.json());
 
