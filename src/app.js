3153ca4c1d49e6973aeaf17f1765540532133abf
Ludhiya <ludhiyargiji1@gmail.com>
2024-07-16 21:41:31 +0800

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 // const crypto = require("crypto");
 // const secret = crypto.randomBytes(64).toString("hex");
 // console.log(secret);
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use("/api", userRoutes);
