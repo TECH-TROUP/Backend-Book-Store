0bb60df0cc34c89d1617b50ae49e1a35c9d7009f
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 22:18:32 +0800

    -

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,29 +2,36 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/bookController");
 const auth = require("../middleware/auth");
-const checkAdmin = require("../middleware/checkAdmin");
+const checkVendor = require("../middleware/checkVendor");
 const upload = require("../middleware/upload");
+const checkAdminOrVendor = require("../middleware/checkAdminOrVendor");
 
 // Book routes
 router.post(
   "/books",
   auth,
-  checkAdmin,
+  checkVendor,
   upload.single("image"),
   bookController.createBook
 );
 router.put(
   "/books/:id",
   auth,
-  checkAdmin,
+  checkVendor,
   upload.single("image"),
   bookController.updateBook
 );
-router.delete("/books/:id", auth, checkAdmin, bookController.deleteBook);
+router.delete(
+  "/books/:id",
+  auth,
+  checkAdminOrVendor,
+  bookController.deleteBook
+);
 router.get("/books", bookController.getAllBooks);
 router.get("/books/:id", bookController.getBookById);
 router.get("/books/category/:categoryId", bookController.getBooksByCategory);
 router.get("/books/search", bookController.searchBooks);
 router.get("/books/filter", bookController.filterBooks);
+router.get("/books/vendor/:vendorId", bookController.getBooksByVendorId);
 
 module.exports = router;
