3860e256afdde2f98c20703bdf9602b46b1a3a9f
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-04 15:10:12 +0800

    added update user role and get users by role

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,13 +2,29 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const auth = require("../middleware/auth");
+const checkAdmin = require("../middleware/checkAdmin");
 
 // User routes
 router.post("/users/register", userController.createUser);
 router.post("/users/login", userController.loginUser);
 router.delete("/users/:id", auth, userController.deleteUser);
+
+router.put(
+  "/users/update-role",
+  auth,
+  checkAdmin,
+  userController.updateUserRole
+);
+
 router.put("/users/:id", auth, userController.updateUser);
 router.get("/users/me", auth, userController.getLoggedInUser);
 router.get("/users/:id", auth, userController.getUserById);
+
+router.get(
+  "/users/roles/:roleId",
+  auth,
+  checkAdmin,
+  userController.getUsersByRoleId
+);
 
 module.exports = router;
