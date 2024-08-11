42fedf5f59584fd56c732db37ef6e12fa897c777
Ludhiya <ludhiyargiji1@gmail.com>
2024-08-11 19:22:34 +0800

    added create payment intent function

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 const auth = require("../middleware/auth");
 const checkAdmin = require("../middleware/checkAdmin");
+const stripe = require("stripe")(
+  "sk_test_51PmYrAKogtyoyt8RTv5G3aQ7AFbIEfu4eusbudh1DX4LmtZMILGdDWqbAh2sCU8sdhdJYQhRnZzWTDYjzQRX2NBF00t806kr1k"
+);
 
 // Payment routes
 router.post("/payments", auth, checkAdmin, paymentController.createPayment);
@@ -21,5 +24,23 @@ router.get(
   checkAdmin,
   paymentController.getPaymentsByUserId
 );
+
+router.post("/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "sgd",
+      payment_method_types: ["card"],
+    });
+
+    res.json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
